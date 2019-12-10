7cef0fa711ad610ee1771b44592eea76ffe8dbc1
IGOLTA <igolta.yt@gmail.com>
2019-12-10 20:15:38 +0100

    Initial upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    $base_dir = "files/";
+
+    function str_replace_first($from, $to, $content){
+        $from = '/'.preg_quote($from, '/').'/';
+
+        return preg_replace($from, $to, $content, 1);
+    }
+
+    function scan_dir_r($dir, &$results = array()){
+        $files = scandir($dir);
+    
+        foreach($files as $key => $value){
+            $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+            if(!is_dir($path)) {
+                $results[] = $path;
+            } else if($value != "." && $value != "..") {
+                scan_dir_r($path, $results);
+                $results[] = $path;
+            }
+        }
+        
+        $files = $results;
+        $results = array();
+
+        foreach($files as $element){
+            if(!is_dir($element)){
+                $element = str_replace_first(realpath(".")."\\", "", $element);
+
+                array_push($results, $element);
+            }
+        }
+
+        return $results;
+    }
+
+    mkdir($base_dir);
+
+    $files = scan_dir_r($base_dir);
+    $json = array();
+
+    $json["filesNumber"] = sizeof($files);
+
+    $checksums = array();
+    foreach($files as $element){
+        $checksums[$element] = md5_file($element);
+    }
+
+    $json["checksums"] = $checksums;
+
+    echo(str_replace("\\\\", "/", json_encode($json)));
+?>
